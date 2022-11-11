ace6cd28d305acaff17a375a5b0b29b8b777a64c
t_cory <Cory.ycj@link8.tech.tw>
2022-11-11 10:46:22 +0800

    feat(首頁): 以CSS background 取代 js img

--- src/page/homepage/index.jsx
+++ src/page/homepage/index.jsx
@@ -1,8 +1,5 @@
 import "./index.scss";
 import React from "react";
-import arrow from "/src/assets/arrow.png";
-import star from "/src/assets/star.png";
-import rect from "/src/assets/rect.png";
 
 export default () => {
   return (
@@ -11,17 +8,16 @@ export default () => {
       <div className="homepage__titleBg" />
       <button className="homepage__applyBtn">
         <span>點擊立刻報名</span>
-        <img src={arrow} alt="" />
       </button>
       <div className="homepage__rightPage">
         <div className="webDesign">WEB DESIGN</div>
-        <img className="star-top" src={star} alt="" />
-        <img className="rect" src={rect} alt="" />
+        <div className="star-top" />
+        <div className="rect" />
         <div className="bug" />
       </div>
       <div className="homepage__leftPage">
         <div className="book-box" />
-        <img className="star-left" src={star} alt="" />
+        <div className="star-left" />
         <div className="fontEnd">FRONT END</div>
       </div>
       <div className="homepage__bottom">

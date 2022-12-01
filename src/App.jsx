859e4d9fb8593968f1ff4f4ef18dfb9cb9827602
t_cory <cory.ycj@outlook.com>
2022-12-01 11:19:35 +0800

    feat(section3): 滾輪動畫導入

--- src/App.jsx
+++ src/App.jsx
@@ -16,6 +16,7 @@ function App() {
         <Section2 />
         <Section3 />
         <Section4 />
+        <Section5 />
       </div>
     </HashRouter>
   );
--- src/page/section3/index.jsx
+++ src/page/section3/index.jsx
@@ -1,11 +1,28 @@
 import "./index.scss";
-import React from "react";
+import React, { useRef, useEffect } from "react";
 import { TextBox } from "@/page/section3/conponent/TextBox";
 import { Background } from "@/page/section3/conponent/Background";
 
 export default () => {
+  const section3Ref = useRef();
+
+  gsap.registerPlugin(ScrollTrigger);
+
+  useEffect(() => {
+    const tl = gsap.timeline({
+      scrollTrigger: {
+        trigger: ".circle-green",
+        start: "top top",
+        end: "+=1750",
+        scrub: 1,
+      },
+    });
+    tl.fromTo(".circle-green", { rotation: -120 }, { rotation: -70 })
+      .to(".circle-green", { rotation: -30 })
+      .to(".circle-green", { rotation: 0 });
+  });
   return (
-    <div className="section3">
+    <div ref={section3Ref} className="section3">
       <Background />
       <TextBox />
     </div>
